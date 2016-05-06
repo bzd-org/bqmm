3263be9f948f0ebc6e6c0e8b4a8116c3e4c9ca10
imbzd <luochuan.wang@gmail.com>
2016-05-06 09:10:23 +0800

    调整BUG、样式

--- application/Api/Controller/GuestbookController.class.php
+++ application/Api/Controller/GuestbookController.class.php
@@ -41,6 +41,50 @@ class GuestbookController extends AppframeController{
 			if ($this->guestbook_model->create()) {
 				$result=$this->guestbook_model->add();
 				if ($result!==false) {
+					$full_name = $_REQUEST['full_name'];
+					$title = $_REQUEST['title'];
+					$email = $_REQUEST['email'];
+					$msg = $_REQUEST['msg'];
+
+					//发送邮件
+					require(VENDOR_PATH."PHPMailer-5.2.14/class.phpmailer.php");
+					require(VENDOR_PATH."PHPMailer-5.2.14/class.smtp.php");
+					$mail = new \PHPMailer();
+			        $mail->IsSMTP();                  // send via SMTP
+			        // $mail->SMTPDebug = 1;
+			        $mail->SMTPAuth = true;           // turn on SMTP authentication
+			        $mail->SMTPSecure = "ssl"; // 安全协议
+			        $mail->Host = "smtp.qq.com";   // SMTP servers
+			        $mail->Port = 465;
+			        $mail->Username = "250175411@qq.com";     // SMTP username  注意：普通邮件认证不需要加 @域名
+			        $mail->Password = "snqsraoqvthgbiaf"; // SMTP password
+			        $mail->From = "250175411@qq.com";      // 发件人邮箱
+			        $mail->FromName =  "表情MM官网-留言板";  // 发件人    
+
+			        $mail->CharSet = "GB2312";   // 这里指定字符集！    
+			        $mail->Encoding = "base64";    
+			        $mail->AddAddress('250175411@qq.com',"似颜绘");  // 收件人邮箱和姓名    
+
+			        $mail->IsHTML(true);
+			         // 邮件主题    
+			        $mail->Subject = $subject;    
+			        // 邮件内容    
+			        $mail->Body = '
+						<html><head>
+						<meta http-equiv="Content-Language" content="zh-cn">
+						<meta http-equiv="Content-Type" content="text/html; charset=GB2312">
+						</head>
+						<body>
+						'.$full_name.'<br/>
+						'.$email.'<br/>
+						'.$title.'<br/>
+						'.$msg.'<br/>
+						</body>
+						</html>
+				    ';
+				    $mail->AltBody ="text/html";
+				    $mail->Send();
+
 					// $this->success("留言成功！");
 					$this->majaxReturn(0, '留言成功！');
 				} else {
